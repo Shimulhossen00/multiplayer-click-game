54e47f062e0b4331a375997712f8f2d8f5fb9795
Shimul Hossen <shimulhossen283@gmail.com>
2026-02-06 22:21:48 +0600

    pdate live chat UI and server logic

--- server.js
+++ server.js
@@ -1,35 +1,26 @@
 const express = require("express");
+
 const app = express();
 const http = require("http").createServer(app);
 const io = require("socket.io")(http);
 
 app.use(express.static("public"));
 
-// Store players and their scores
-let players = {};
+io.on("connection", (socket) =>{
 
-io.on("connection", (socket) => {
-  console.log("Player connected:", socket.id);
+  console.log("user connected", socket.id);
 
-  // Add player
-  players[socket.id] = { score: 0 };
-  io.emit("players", players);
-
-  // When player clicks button
-  socket.on("click", () => {
-    players[socket.id].score++;
-    io.emit("players", players); // send updated scores to everyone
+  socket.on("chat message", (msg) =>{
+    io.emit("chat message", {id: socket.id, msg: msg});
   });
 
-  // When player disconnects
-  socket.on("disconnect", () => {
-    delete players[socket.id];
-    io.emit("players", players);
-    console.log("Player disconnected:", socket.id);
-  });
+  socket.on("disconnect", () =>{
+    console.log("user disconnect:",socket.id);
+  })
 });
 
-const PORT = process.env.PORT || 3000;
-http.listen(PORT, () => {
-  console.log("Server running at http://localhost:" + PORT);
+const port = process.env.PORT || 3000;
+
+http.listen(port, () =>{
+  console.log("Server running at http://localhost:" + port);
 });
